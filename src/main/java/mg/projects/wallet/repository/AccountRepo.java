89a8c627962bd7bd154f27897c2e4449cc2fd870
myranto <95678823+myranto@users.noreply.github.com>
2025-04-09 08:11:50 +0300

    update account

--- src/main/java/mg/projects/wallet/repository/AccountRepo.java
+++ src/main/java/mg/projects/wallet/repository/AccountRepo.java
@@ -1,8 +1,14 @@
 package mg.projects.wallet.repository;
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import mg.projects.wallet.common.CommonRepository;
 import mg.projects.wallet.models.Account;
 
 public interface AccountRepo extends CommonRepository<Account, String> {
+    @Query("select a from Account a where a.status=0 and a.customer_id = :customer_id")
+    List<Account> findByCustomer_id(String customer_id);
 }
